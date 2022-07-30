8ec457d675f9726134d5e0c487e8eb0c89218da8
Karuncj <83808984+Karuncj@users.noreply.github.com>
2022-07-30 13:31:37 +0530

    Update grades.php

--- grades.php
+++ grades.php
@@ -15,21 +15,28 @@ if($data==false){
 }
 else{
     
-}
-
+}   
+    $data_sem=$_POST['semester'];
     $data_admno=$_POST['admission'];
-    $data_gt=$_POST['gt'];
-    $data_os=$_POST['os'];
-    $data_coa=$_POST['coa'];
-    $data_coi=$_POST['coi'];
-    $data_pe=$_POST['pe'];
-    $data_dbms=$_POST['dbms'];
+    $data_sub1=$_POST['subject_1'];
+    $data_sub2=$_POST['subject_2'];
+    $data_sub3=$_POST['subject_3'];
+    $data_sub4=$_POST['subject_4'];
+    $data_sub5=$_POST['subject_5'];
+    $data_sub6=$_POST['subject_6'];
+    $data_sname1=$_POST['sname_1'];
+    $data_sname2=$_POST['sname_2'];
+    $data_sname3=$_POST['sname_3'];
+    $data_sname4=$_POST['sname_4'];
+    $data_sname5=$_POST['sname_5'];
+    $data_sname6=$_POST['sname_6'];
 
-    $sql="INSERT INTO `grades`(`adm_no`,`graph_theory`,`operating_system`,`database_management_system`,`computer_organization_and_architecture`,`professional_ethic`,`constitution_of_india`) VALUES('$data_admno','$data_gt','$data_os','$data_dbms','$data_coa','$data_pe',' $data_coi')";
-    $result=mysqli_query($data,$sql);
-
+    $sql1="INSERT INTO `grades`(`semester`,`adm_no`,`subject1`,`subject2`,`subject3`,`subject4`,`subject5`,`subject6`) VALUES('$data_sem','$data_admno','$data_sub1','$data_sub2','$data_sub3','$data_sub4','$data_sub5',' $data_sub6')";
+    $sql2="INSERT INTO `subject`(`adm_no`,`sub_1`,`sub_2`,`sub_3`,`sub_4`,`sub_5`,`sub_6`) VALUES('$data_admno','$data_sname1','$data_sname2','$data_sname3','$data_sname4','$data_sname5',' $data_sname6')";
+    $result1=mysqli_multi_query($data,$sql1);
+    $result2=mysqli_query($data,$sql2);
   
-    if($result){
+    if($result1 && $result2){
         header("Location:main.html?UploadSuccess");
     }
     else{
