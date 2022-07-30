5794bed9ce8ae5ff76b64a4e9cddf961844156b9
Karuncj <83808984+Karuncj@users.noreply.github.com>
2022-07-30 14:31:13 +0530

    Update updateGrade.php

--- updateGrade.php
+++ updateGrade.php
@@ -17,19 +17,28 @@ else{
     
 }
 
+    $data_sem=$_POST['semester'];
     $data_admno=$_POST['admission'];
-    $data_gt=$_POST['gt'];
-    $data_os=$_POST['os'];
-    $data_coa=$_POST['coa'];
-    $data_coi=$_POST['coi'];
-    $data_pe=$_POST['pe'];
-    $data_dbms=$_POST['dbms'];
+    $data_sub1=$_POST['subject_1'];
+    $data_sub2=$_POST['subject_2'];
+    $data_sub3=$_POST['subject_3'];
+    $data_sub4=$_POST['subject_4'];
+    $data_sub5=$_POST['subject_5'];
+    $data_sub6=$_POST['subject_6'];
+    $data_sname1=$_POST['sname_1'];
+    $data_sname2=$_POST['sname_2'];
+    $data_sname3=$_POST['sname_3'];
+    $data_sname4=$_POST['sname_4'];
+    $data_sname5=$_POST['sname_5'];
+    $data_sname6=$_POST['sname_6'];
 
-    $sql="update `grades` set `graph_theory`='$data_gt',`operating_system`='$data_os',`database_management_system`='$data_dbms',`computer_organization_and_architecture`='$data_coa',`professional_ethic`='$data_pe',`constitution_of_india`='$data_coi' where adm_no='$data_admno' ";
-    $result=mysqli_query($data,$sql);
-
+    $sql1="update `grades` set `semester`='$data_sem',`subject1`='$data_sub1',`subject2`='$data_sub2',`subject3`='$data_sub3',`subject4`='$data_sub4',`subject5`='$data_sub5',`subject6`='$data_sub6' where adm_no='$data_admno'";
+    $sql2="update `subject` set `sub_1`='$data_sname1',`sub_2`='$data_sname2',`sub_3`='$data_sname3',`sub_4`='$data_sname4',`sub_5`='$data_sname5',`sub_6`='$data_sname6' where adm_no='$data_admno' ";    
+            
+    $result1=mysqli_multi_query($data,$sql1);
+    $result2=mysqli_query($data,$sql2);
   
-    if($result){
+    if($result1 && $result2){
         header("Location:main.html?UploadSuccess");
     }
     else{
