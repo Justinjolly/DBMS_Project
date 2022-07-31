eca5a65e2d2efd47766ff49e6a63a0e9aeb5416e
Karuncj <83808984+Karuncj@users.noreply.github.com>
2022-07-31 23:09:38 +0530

    Update delete_student.php

--- delete_student.php
+++ delete_student.php
@@ -12,11 +12,18 @@ $conn = new mysqli("localhost", $username, $password, $database);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
+try{
 $id=$_REQUEST['id'];
 $query = "DELETE FROM registration WHERE admissonNo='$id'"; 
 $result = mysqli_query($conn,$query) ;
 header("Location: view_personal_details.php"); 
+}
+catch(Exception $e){
+  echo '<script type ="text/JavaScript">';  
+  echo 'alert("Cannot delete because of Foriegn key constraints")';  
+  echo '</script>';  
+  echo "<script> location.href='view_personal_details.php'; </script>";
+}
 $conn->close();
  ?>
 
